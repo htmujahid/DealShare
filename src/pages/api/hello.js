c9b2b502e72373838db70e24e3633ad439f006f0
htmujahid <htmujahid@gmail.com>
2023-02-03 19:35:34 +0500

    add superuser

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { database } from "@/lib/api/middleware";
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    database(req, res);
+    res.status(200).json({ name: "John Doe" });
 }
