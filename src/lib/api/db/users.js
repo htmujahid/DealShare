c9da742616d57f41f777d036ccb3fda472959cd4
Altaf Ahmad <48713462+altafahmad1@users.noreply.github.com>
2023-02-08 14:26:43 +0500

    add authentitcation, user sign up (#6)
    * add authentitcation, user sign up
    
    * Fix small error
    
    * resolve conflicts in package-lock
    
    * remove validator package

--- src/lib/api/db/users.js
+++ src/lib/api/db/users.js
@@ -2,28 +2,43 @@ import bcrypt from "bcryptjs";
 import { db } from "../middleware";
 
 export async function getUserByEmail(email) {
-    return db
-        .collection("users")
-        .findOne({ email }, { projection: dbProjectionUsers() })
-        .then((user) => user || null);
+  return db
+    .collection("users")
+    .findOne({ email }, { projection: dbProjectionUsers() })
+    .then((user) => user || null);
 }
 
 export function dbProjectionUsers(prefix = "") {
-    return {
-        [`${prefix}password`]: 0,
-        [`${prefix}emailVerified`]: 0,
-    };
+  return {
+    [`${prefix}password`]: 0,
+    [`${prefix}emailVerified`]: 0,
+  };
+}
+
+export async function addUser(user) {
+  let password = await bcrypt.hash(user.password, 10);
+
+  const newUser = {
+    ...user,
+    password,
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  };
+
+  const { insertedId } = await db.collection("users").insertOne(newUser);
+
+  return insertedId;
 }
 
 export async function addSuperAdmin() {
-    const password = await bcrypt.hash(process.env.SEED_ADMIN_PASS, 10);
-    const user = {
-        firstName: "Super",
-        lastName: "Admin",
-        email: process.env.SEED_ADMIN_EMAIL,
-        password,
-        role: "admin",
-        emailVerified: true,
-    };
-    return await db.collection("users").insertOne(user);
+  const password = await bcrypt.hash(process.env.SEED_ADMIN_PASS, 10);
+  const user = {
+    firstName: "Super",
+    lastName: "Admin",
+    email: process.env.SEED_ADMIN_EMAIL,
+    password,
+    role: "admin",
+    emailVerified: true,
+  };
+  return await db.collection("users").insertOne(user);
 }
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,82 @@
+import { createRouter } from 'next-connect';
+import { database, db } from '@/lib/api/middleware';
+import { verifyPassword } from '@/lib/app/user/utils';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default async function auth(req, res) {
+	const router = createRouter();
+
+	if (
+		req.query.nextauth.includes('credentials') ||
+		req.query.nextauth.includes('signin')
+	) {
+		router.use(database).us;
+		e(() => null);
+		await router.run(req, res);
+	}
+	return await NextAuth(req, res, {
+		providers: [
+			CredentialsProvider({
+				async authorize(credentials) {
+					const { email, password } = credentials;
+
+					const user = await db.collection('users').findOne({
+						email,
+					});
+
+					if (!user)
+						throw new Error(
+							'Account not found. Please create an account to use Dealshare.'
+						);
+
+					const isPasswordValid = await verifyPassword(password, user.password);
+					if (!isPasswordValid)
+						throw new Error(
+							'Incorrect Password. Please try again with correct credentials.'
+						);
+
+					return {
+						id: user._id.toString(),
+						email: user.email,
+						firstName: user.firstName,
+						lastName: user.lastName,
+						role: user.role,
+					};
+				},
+			}),
+		],
+
+		pages: {
+			error: '/login',
+		},
+
+		session: {
+			strategy: 'jwt',
+		},
+
+		callbacks: {
+			async jwt({ token, user }) {
+				if (user) {
+					token.firstName = user.firstName;
+					token.lastName = user.lastName;
+					token.role = user.role;
+				}
+
+				return token;
+			},
+
+			async session({ session, token }) {
+				session.user.id = token.sub;
+				session.user.firstName = token.firstName;
+				session.user.lastName = token.lastName;
+				session.user.role = token.role;
+
+				return session;
+			},
+		},
+
+		debug: true,
+		secret: process.env.SECRET,
+	});
+}
