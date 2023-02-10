a4856e177c5f6c92abc2184b394df76fcecf943b
Talha Mujahid <htmujahid@gmail.com>
2023-02-10 09:17:04 +0500

    initial structure of admin panel (#12)

---
+++ src/pages/admin/index.js
@@ -0,0 +1,10 @@
+import { DashboardComponents } from "@/sections/Admin";
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <DashboardComponents />
+    </div>
+  );
+}
