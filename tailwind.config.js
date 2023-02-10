d831d828bc21713d2f1273be44f8ac2ad9f80f82
htmujahid <htmujahid@gmail.com>
2023-02-10 09:15:59 +0500

    initial structure of admin panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: {
-                    DEFAULT: "#B9E6E8",
-                    dark: "#19B2B8",
-                },
-                secondary: {
-                    DEFAULT: "#F2F2F2",
-                },
-                black: {
-                    DEFAULT: "#151515",
-                },
-                white: {
-                    DEFAULT: "#FCFFFD",
-                },
-                neutral: {
-                    light: "#F9F9F9",
-                    DEFAULT: "#E4EAEE",
-                },
-            },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#B9E6E8",
+          dark: "#19B2B8",
         },
-        container: {
-            padding: {
-                DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "5rem",
-                "2xl": "6rem",
-            },
+        secondary: {
+          DEFAULT: "#F2F2F2",
         },
+        black: {
+          DEFAULT: "#151515",
+        },
+        white: {
+          DEFAULT: "#FCFFFD",
+        },
+        neutral: {
+          light: "#F9F9F9",
+          DEFAULT: "#E4EAEE",
+        },
+      },
     },
-    plugins: [],
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
 };
