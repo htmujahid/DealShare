6e61d0f3f0e404bc5498a2ebae391ba4a0e4804c
htmujahid <htmujahid@gmail.com>
2023-05-30 07:58:22 +0500

    product ui changed

--- src/components/Card/Product/ProductCard.jsx
+++ src/components/Card/Product/ProductCard.jsx
@@ -16,6 +16,58 @@ function ProductCard({ product }) {
 
   return (
     <Link className="col-span-1" href={`/product/${product?._id}`}>
+      <div className="relative">
+        <div className="relative h-72 w-full overflow-hidden rounded-lg">
+          <img
+            src={product?.media?.mediaUrl[0] ?? ""}
+            alt={product?.name}
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+        <div className="relative mt-4">
+          <h3 className="text-sm font-medium text-gray-900">{product?.name}</h3>
+          <p className="mt-1 text-sm text-gray-500">{product?.category}</p>
+        </div>
+        <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+          />
+          <p className="relative text-lg font-semibold text-white">
+            ${product?.sellingPrice}
+          </p>
+        </div>
+      </div>
+      <div className="mt-6">
+        {showConfirmationModal && (
+          <ConfirmationModal
+            numberOfItems={cartItems.length}
+            onClose={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setShowConfirmationModal(false);
+            }}
+          />
+        )}
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (!isItemAlreadyIncluded(cartItems, product)) {
+              setCartItems((prev) => [...prev, product]);
+            }
+            setShowConfirmationModal(true);
+          }}
+          className="relative w-full flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+        >
+          Add to bag<span className="sr-only">, {product?.name}</span>
+        </button>
+      </div>
+    </Link>
+  );
+
+  return (
+    <Link className="col-span-1" href={`/product/${product?._id}`}>
       <div className="col-span-4 p-4 border border-primary-light rounded-xl">
         <div className="flex items-center justify-center rounded-xl max-h-[150px] max-h-[150px]">
           <img
