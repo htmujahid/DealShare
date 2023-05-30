78c10ece37c919b6baeca42abb560d1bebeb08d6
htmujahid <htmujahid@gmail.com>
2023-05-30 08:11:02 +0500

    product ui changed

--- src/components/Card/Product/ProductCard.jsx
+++ src/components/Card/Product/ProductCard.jsx
@@ -24,9 +24,9 @@ function ProductCard({ product }) {
             className="h-full w-full object-cover object-center"
           />
         </div>
-        <div className="relative mt-4 h-10 overflow-hidden">
-          <h3 className="text-sm font-medium text-gray-900">{product?.name}</h3>
-          <p className="mt-1 text-sm text-gray-500">{product?.category}</p>
+        <div className="relative mt-4">
+          <h3 className="text-sm font-medium text-gray-900 h-5 overflow-hidden">{product?.name}</h3>
+          <p className="mt-1 text-sm text-gray-500 h-5 overflow-hidden">{product?.category}</p>
         </div>
         <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
           <div
@@ -38,7 +38,7 @@ function ProductCard({ product }) {
           </p>
         </div>
       </div>
-      <div className="mt-6">
+      <div className="mt-4">
         {showConfirmationModal && (
           <ConfirmationModal
             numberOfItems={cartItems.length}
