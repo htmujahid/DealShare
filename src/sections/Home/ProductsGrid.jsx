580320dd49431d3551b2f8948bb4f0805161807b
htmujahid <htmujahid@gmail.com>
2023-05-30 08:12:58 +0500

    product ui changed

--- src/sections/Home/ProductsGrid.jsx
+++ src/sections/Home/ProductsGrid.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function ProductsGrid({ products }) {
   return (
     <CustomerContainer>
-      <div className="grid grid-cols-5 justify-center flex-wrap gap-2 py-16">
+      <div className="grid grid-cols-5 justify-center flex-wrap gap-4 py-16">
         {products?.map((product) => (
           <ProductCard product={product} key={product._id} />
         ))}
